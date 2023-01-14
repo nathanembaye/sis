6dca7f62a747b404bd2b91efac7a71f6bacb33b5
nathanembaye <nathanembaye26@gmail.com>
2023-01-13 20:47:46 -0500

    setup gh-pages

--- components/layout.js
+++ components/layout.js
@@ -86,7 +86,7 @@ export default function Layout({ children }) {
           </a>
           <a
             className="text-gray-400 dark:text-white hover:text-lightBlue-600 dark:hover:text-pink-500"
-            href="https://twitter.com/katherinecodes"
+            href="https://twitter.com/sharon_emb"
             target="_blank"
           >
             <FontAwesomeIcon className="mr-6 text-2xl" icon={faTwitter} />
